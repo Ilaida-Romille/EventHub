9e2d803424c5daab6a83aa412402acd0587a1c75
Ivan Cadiang <ivan.cadiang@pointwest.com>
2026-07-23 13:43:48 +0800

    feat: migrate login page to Angular

---
+++ sandbox/front-end/eventhub/src/app/login/login.component.ts
@@ -0,0 +1,18 @@
+import { Component, inject, signal } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [ReactiveFormsModule, RouterLink],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+})
+export class LoginComponent {
+  private fb = inject(FormBuilder);
+
+  loginForm = this.fb.nonNullable.group({});
+
+  onSubmit(): void {}
+}
