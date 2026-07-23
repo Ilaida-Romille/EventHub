7ca1141051bb72ec66b1945fd1e10377e9d1e410
Ivan Cadiang <ivan.cadiang@pointwest.com>
2026-07-23 14:53:09 +0800

    feat: add dashboard route to app routing

--- sandbox/front-end/eventhub/src/app/app.routes.ts
+++ sandbox/front-end/eventhub/src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './login/login.component';
+import { DashboardComponent } from './platform/dashboard/dashboard.component';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
+  { path: 'platform/dashboard', component: DashboardComponent},
 
   { path: '', redirectTo: 'login', pathMatch: 'full' } 
 ];
