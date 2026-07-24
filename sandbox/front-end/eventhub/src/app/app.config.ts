d5e3bb0f85e900b37a084603f9be807a2c97676b
Ivan Cadiang <ivan.cadiang@pointwest.com>
2026-07-24 10:27:10 +0800

    feat: add shared Lucide icon component and providers

--- sandbox/front-end/eventhub/src/app/app.config.ts
+++ sandbox/front-end/eventhub/src/app/app.config.ts
@@ -1,11 +1,56 @@
 import { ApplicationConfig, provideBrowserGlobalErrorListeners } from '@angular/core';
+import {
+   LucideArrowRight,
+   LucideBuilding2,
+   LucideChevronDown,
+   LucideChevronLeft,
+   LucideChevronRight,
+   LucideCircleCheckBig,
+   LucideLayoutDashboard,
+   LucideLogOut,
+   LucideMessagesSquare,
+   LucideMoon,
+   LucidePaperclip,
+   LucidePanelLeftClose,
+   LucidePanelLeftOpen,
+   LucideReceiptText,
+   LucideSendHorizontal,
+   LucideSun,
+   LucideTicket,
+   LucideUserRound,
+   LucideUsers,
+   LucideX,
+   provideLucideIcons
+} from '@lucide/angular';
 import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [
-    provideBrowserGlobalErrorListeners(),
-    provideRouter(routes)
-  ]
+   providers: [
+      provideBrowserGlobalErrorListeners(),
+      provideRouter(routes),
+      provideLucideIcons(
+         LucideArrowRight,
+         LucideBuilding2,
+         LucideChevronDown,
+         LucideChevronLeft,
+         LucideChevronRight,
+         LucideCircleCheckBig,
+         LucideLayoutDashboard,
+         LucideLogOut,
+         LucideMessagesSquare,
+         LucideMoon,
+         LucidePaperclip,
+         LucidePanelLeftClose,
+         LucidePanelLeftOpen,
+         LucideReceiptText,
+         LucideSendHorizontal,
+         LucideSun,
+         LucideTicket,
+         LucideUserRound,
+         LucideUsers,
+         LucideX
+      )
+   ]
 };
---
+++ sandbox/front-end/eventhub/src/app/shared/components/lucide-icon/lucide-icon.component.ts
@@ -0,0 +1,15 @@
+import { Component, input } from '@angular/core';
+import { LucideDynamicIcon } from '@lucide/angular';
+
+@Component({
+   selector: 'app-lucide-icon',
+   standalone: true,
+   imports: [LucideDynamicIcon],
+   templateUrl: './lucide-icon.component.html'
+})
+export class LucideIconComponent {
+   readonly name = input.required<string>();
+   readonly size = input<number>(18);
+   readonly strokeWidth = input<number>(2);
+   readonly iconClass = input<string>('');
+}
