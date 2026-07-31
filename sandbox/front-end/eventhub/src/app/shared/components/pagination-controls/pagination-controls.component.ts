b43f7ac8022d3d88e89ecffb13cf28156358333e
Ivan Cadiang <ivan.cadiang@pointwest.com>
2026-07-31 14:53:16 +0800

    refactor: improve pagination UI/UX

--- sandbox/front-end/eventhub/src/app/shared/components/pagination-controls/pagination-controls.component.ts
+++ sandbox/front-end/eventhub/src/app/shared/components/pagination-controls/pagination-controls.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, input, output } from '@angular/core';
 import { LucideIconComponent } from '../lucide-icon/lucide-icon.component';
 
 @Component({
@@ -14,4 +14,24 @@ export class PaginationControlsComponent {
    readonly indicatorText = input<string>('Page 1 of 1');
    readonly align = input<'start' | 'center' | 'end'>('end');
    readonly withBottomMargin = input<boolean>(false);
+
+   readonly isFirstPage = input<boolean>(false);
+   readonly isLastPage = input<boolean>(false);
+
+   readonly prevClick = output<void>();
+   readonly nextClick = output<void>();
+
+   protected onPrev(event: MouseEvent): void {
+      event.preventDefault();
+      if (!this.isFirstPage()) {
+         this.prevClick.emit();
+      }
+   }
+
+   protected onNext(event: MouseEvent): void {
+      event.preventDefault();
+      if (!this.isLastPage()) {
+         this.nextClick.emit();
+      }
+   }
 }
